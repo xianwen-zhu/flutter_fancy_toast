453c17c57a62fb956627b56f20d007405100d539
朱先文 <>
2024-07-31 16:45:16 +0800

    Android问题修复

--- android/src/main/kotlin/com/example/fancy_toast/FancyToastManager.kt
+++ android/src/main/kotlin/com/example/fancy_toast/FancyToastManager.kt
@@ -14,6 +14,7 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
@@ -97,6 +98,7 @@ class FancyToastManager private constructor() {
             setContentView(R.layout.toast_loading)
             setCancelable(false) // 设置为不可取消
             window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+            window?.clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND)
             window?.setGravity(Gravity.CENTER)
 
             val gifImageView = findViewById<ImageView>(R.id.loading_image)
